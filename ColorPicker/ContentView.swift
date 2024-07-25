4176da11a1204c63e08ea50c3f87e7315dbf95af
consuelita <adelita@operatorfoundation.org>
2024-07-25 14:15:37 -0500

    SwiftUI Views & Layouts: Building Views part 2

--- ColorPicker/ContentView.swift
+++ ColorPicker/ContentView.swift
@@ -14,27 +14,23 @@ struct ContentView: View
     @State private var blueValue = 0.0
     @State private var activeColor = Color.white
     
-    @Environment(\.horizontalSizeClass) var horizontalSizeClass
-    @Environment(\.verticalSizeClass) var verticalSizeClass
-    
-    var landscapeIsCompact: Bool
-    {
-        horizontalSizeClass == .compact && verticalSizeClass == .compact ||
-        horizontalSizeClass == .regular && verticalSizeClass == .compact
-    }
-    
-    var layout: AnyLayout {
-        landscapeIsCompact ? AnyLayout(HStackLayout(spacing: 16)) : AnyLayout(VStackLayout(spacing: 16))
-    }
-    
     var body: some View 
     {
         let _ = Self._printChanges()
         let _ = print("red value = \(redValue)")
         
-        layout {
-            ColorCardView(color: activeColor)
-            RGBSliderStackView(color: $activeColor, red: $redValue, green: $greenValue, blue: $blueValue)
+        ViewThatFits {
+            PortraitColorPicker(
+                color: $activeColor,
+                red: $redValue,
+                green: $greenValue,
+                blue: $blueValue)
+            
+            LandscapeColorPicker(
+                color: $activeColor,
+                red: $redValue,
+                green: $greenValue,
+                blue: $blueValue)
         }
         .padding()
     }
